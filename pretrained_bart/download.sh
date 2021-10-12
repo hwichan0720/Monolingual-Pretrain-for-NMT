454d309683219c0811443c8741bd0d29676cc1be
hwichan0720 <2mmhwichan@gmail.com>
2021-10-12 12:45:18 +0900

    update

--- pretrained_bart/download.sh
+++ pretrained_bart/download.sh
@@ -7,7 +7,3 @@ tar -zxvf japanese_bart_base_1.1.tar.gz
 echo "download english bart"
 wget https://dl.fbaipublicfiles.com/fairseq/models/bart.base.tar.gz
 tar -zxvf bart.base.tar.gz
-
-echo "download mbart"
-wget https://dl.fbaipublicfiles.com/fairseq/models/mbart/mbart.cc25.v2.tar.gz
-tar -zxvf /mbart.cc25.v2.tar.gz
